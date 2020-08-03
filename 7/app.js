af72dad8ccb8211163ea926a72975b02d292215d
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-08-03 21:27:31 +0300

    lectia 15

---
+++ 7/app.js
@@ -0,0 +1,21 @@
+const h1 = document.querySelector("h1");
+const body = document.body;
+const input = document.querySelector("input");
+// const listItemElement = document.querySelectorAll("li");
+const listItemElement = document.getElementsByTagName("li");
+const ul = document.querySelector("ul");
+
+h1.textContent = "Some new text";
+h1.className = "title";
+h1.style.color = "blue";
+h1.style.backgroundColor = "red";
+
+input.value = "Some other input";
+
+for (const listItemEl of listItemElement) {
+  console.dir(listItemEl);
+}
+
+body.style.backgroundColor = "#f4f4f4";
+
+console.log(ul.childNodes);
