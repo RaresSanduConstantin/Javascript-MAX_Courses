f8e7dbfe948a34e7e75097357e876a859965183a
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-08-15 11:10:12 +0300

    input display

--- 7/app.js
+++ 7/app.js
@@ -1,18 +1,22 @@
 const h1 = document.querySelector("h1");
 const body = document.body;
-const input = document.querySelector("input");
+// const input = document.querySelector("input");
 // const listItemElement = document.querySelectorAll("li");
 const listItemElement = document.getElementsByTagName("li");
 const ul = document.querySelector("ul");
+
+const input = document.getElementById("input-text");
+const submit = document.getElementById("input-submit");
 
 h1.textContent = "Some new text";
 h1.className = "title";
 h1.style.color = "blue";
 h1.style.backgroundColor = "red";
 
-input.value = "Some other input";
+input.placeholder = "Some other input";
 
 for (const listItemEl of listItemElement) {
+  listItemEl.style.backgroundColor = "#F0FFFF";
   console.dir(listItemEl);
 }
 
@@ -23,4 +27,14 @@ console.log(ul.childNodes);
 // document.documentElement.parentElement
 // document.getElementsByTagName(body);
 
-console.log("123123123");
+function subtmiText() {
+  let text = input.value;
+  let h = document.createElement("p");
+  let t = document.createTextNode(text);
+  h.appendChild(t);
+  document.body.appendChild(h);
+}
+
+// function submitText(input, button) {}
+
+// document.addEventListener("click", submitText);
